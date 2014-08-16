25bc4ec32269741693bb18b49f2fe81a4b659003
Yan Justino <yanlimaj@gmail.com>
2014-08-16 10:13:21 -0300

    Removendo Classes não utilizadas

--- ExemplosCSharp.Console/Exemplos/ExemploThreads.cs
+++ ExemplosCSharp.Console/Exemplos/ExemploThreads.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
 using System.Threading;
-using System.Threading.Tasks;
 using console = System.Console;
 
 namespace ExemplosCSharp.Console.Exemplos
