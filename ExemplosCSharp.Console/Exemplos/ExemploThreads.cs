33c5a969b0e6e89fc507372f49d054852ce75029
Yan Justino <yanlimaj@gmail.com>
2014-08-16 09:53:17 -0300

    Exemplo com Thread

---
+++ ExemplosCSharp.Console/Exemplos/ExemploThreads.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExemplosCSharp.Console.Exemplos
+{
+  public static class ExemploThreads
+  {
+    public static void Executar()
+    {
+      var array = new int[] { 1, 4, 18, 32, 46, 98, 115 };
+      array.Minimo();
+      array.Maximo();
+    }
+
+    public static void Minimo(this int[] array)
+    {
+      Thread thread = new Thread(RecuperarMinimo);
+      thread.Start(array);
+    }
+
+    public static void Maximo(this int[] array)
+    {
+      Thread thread = new Thread(RecuperarMaximo);
+      thread.Start(array);
+    }
+
+    private static void RecuperarMinimo(object array)
+    {
+      var a = (array as int[]);
+      System.Console.WriteLine("Valor Mínimo é: " + a.Min());
+    }
+
+    private static void RecuperarMaximo(object array)
+    {
+      var a = (array as int[]);
+      System.Console.WriteLine("Valor Máximo é: " + a.Max());
+    }
+  }
+}
---
+++ ExemplosCSharp.Console/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemplosCSharp.Console
+{
+  class Program
+  {
+    static bool continuar = true;
+
+    static void Main(string[] args)
+    {
+      while(continuar)
+      {
+        DesenharMenu();
+        ExecutarOpcaoDoMenu();
+      }
+    }
+
+    static void DesenharMenu()
+    {
+      System.Console.WriteLine();
+      System.Console.WriteLine("Digite 1 parar recuperar o mínimo e o máximo");
+      System.Console.WriteLine("Digite 99 parar sair");
+    }
+
+    static void ExecutarOpcaoDoMenu()
+    {
+      int opcao = 99;
+      int.TryParse(System.Console.ReadLine(), out opcao);
+
+      switch (opcao)
+      {
+        case 1: Exemplos.ExemploThreads.Executar(); break;
+        default: continuar = !opcao.Equals(99); break;
+      }
+    }
+  }
+}
